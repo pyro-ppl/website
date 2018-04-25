c1330ee7de5cc753d609fe176295edd197ddf9d5
jpchen <jonathanp.chen@gmail.com>
2018-04-24 18:26:02 -0700

    0.2 tutorials

---
+++ examples/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.2.0-a0+88254ac',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
